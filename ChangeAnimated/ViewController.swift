edbacf14c2ebd05fccd4a60e96e47be8fa03b8e2
yaosixu <jason_yaosixu@163.com>
2016-06-06 11:53:18 +0800

    Initial Commit

---
+++ ChangeAnimated/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChangeAnimated
+//
+//  Created by yaosixu on 16/6/6.
+//  Copyright © 2016年 Ysx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
